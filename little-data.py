54174f1c05812a34b71a016d9e413f57adc5b77e
trgamese <trgamese@outlook.com>
2024-10-09 07:36:39 +0000

    update little data

--- little-data.py
+++ little-data.py
@@ -1,68 +1,52 @@
+import os
 import atlite
-import cartopy.io.shapereader as shpreader
-import geopandas as gpd
-from shapely.geometry import box
-import pandas as pd
 import logging
-from argparse import ArgumentParser
-from datetime import datetime
+import platform
 
-##
+url_content = "url: https://cds.climate.copernicus.eu/api\nkey: 88edbff7-a215-49d4-8423-286cccd04872"
 
-print(f'0 : {datetime.now()}')
+if platform.system() == "Linux":
+    file_path = os.path.expanduser("~/.cdsapirc")
+elif platform.system() == "Windows":
+    username = os.getlogin()
+    file_path = f"C:\\Users\\{username}\\.cdsapirc"
+else:
+    raise Exception("Unsupported operating system")
+
+if not os.path.exists('./tmp'):
+    os.mkdir('./tmp')
+
+with open(file_path, "w") as file:
+    file.write(url_content)
 
 year = '2011'
 
 logging.basicConfig(level=logging.INFO)
 
-world = gpd.read_file("./data/ne_110m_admin_0_countries.shp")
-
-# Drop uninhabited regions and Antarctica
-# world = world[(world.POP_EST > 0) & (world.NAME != "Antarctica")]
-
-# region = world
-region_name = "western-europe"
-
-# Loop over the years
-logging.info(f"Processing {year}")
-
-##
-
-print(f'1 : {datetime.now()}')
-
-# Define the cutout; this will not yet trigger any major operations
+# Define the cutout; URL and key can be set here
 cutout = atlite.Cutout(
-    path=f"{region_name}-{year}-01.nc",
-    module="era5", 
-    # bounds=region.unary_union.bounds, 
+    path=f"western-europe-{year}-01.nc",
+    module="era5",
     x=slice(-13.6913, 1.7712),
     y=slice(49.9096, 60.8479),
     time=f"{year}-01",
 )
 
-##
-
-print(cutout)
-
-##
-
-# This is where all the work happens (this can take some time).
+# Prepare the cutout (this can take some time).
 cutout.prepare(
     compression={"zlib": True, "complevel": 9},
-    features = ['wind',],
+    features=['wind'],
     monthly_requests=True,
     concurrent_requests=True,
-    overwrite=True)
-
-print(f'2 : {datetime.now()}')
-
-##
+    tmpdir='./tmp'
+)
 
 # Extract the wind power generation capacity factors
 wind_power_generation = cutout.wind(
-    "Vestas_V112_3MW", 
+    "Vestas_V112_3MW",
     capacity_factor_timeseries=True,
-    )
+)
 
-print(f'3 : {datetime.now()}')
 print(wind_power_generation)
+
+input('Press Enter...')
